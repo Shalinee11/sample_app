db50652e275e4ce6a3da5cef6fa3ae823d801e51
shalinee <shalinee11052012@gmail.com>
2015-06-20 02:55:16 +0000

    Add password resets & email configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,8 +41,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { host: 'example.com' }
   host = 'example.com'
   config.action_mailer.default_url_options = { host: host }
-  
+
 end
